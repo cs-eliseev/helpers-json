85477d91ccbc6a67229c37093725343ebd87307e
cs-eliseev <a.k.eliseev@gmail.com>
2019-01-18 21:41:00 +0300

    add json encode example

---
+++ examples/examples-json.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+
+use cse\helpers\Exceptions\CSEHelpersJsonException;
+use cse\helpers\Json;
+
+// Example: encode
+// ["example" => 12345] => {"example": 12345}
+var_dump(Json::encode(["example" => 12345]));
+echo PHP_EOL;
