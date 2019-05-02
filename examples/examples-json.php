9963f65e8209237b78a39f3e8191cee616f0883b
cs-eliseev <a.k.eliseev@gmail.com>
2019-05-02 06:36:20 +0300

    add label info

--- examples/examples-json.php
+++ examples/examples-json.php
@@ -6,18 +6,48 @@ use cse\helpers\Exceptions\CSEHelpersJsonException;
 use cse\helpers\Json;
 
 // Example: encode
+$label = 'Encode: ';
 // ['example' => 12345] => {"example": 12345}
-var_dump(Json::encode(['example' => 12345]));
+var_dump($label . Json::encode(['example' => 12345]));
 echo PHP_EOL;
 
 // Example: petty print
-// ["example" => 12345, "example2" => 56789] => {"example": 12345, "example": 56789}
-var_dump(Json::prettyPrint(['example' => 12345, 'example2' => 56789]));
+$label = 'Petty print: ';
+// ["example" => 12345, "example2" => 56789] => {"example": 12345, "example2": 56789}
+var_dump($label . Json::prettyPrint(['example' => 12345, 'example2' => 56789]));
+// test => "test"
+var_dump($label . Json::prettyPrint('test'));
 echo PHP_EOL;
 
 // Example: decode
+$label = 'Decode: ';
 // {"example": 12345} => ["example" => 12345]
+echo $label . PHP_EOL;
 var_dump(Json::decode('{"example": 12345}'));
+echo PHP_EOL;
+
+// Example: set
+$label = 'Set: ';
+// {"example": 12345} => {"example": 12345, "example2": 56789}
+var_dump($label . Json::set('{"example": 12345}', 'example2', 56789));
+// {"example": 12345} => {"example": 56789}
+var_dump($label . Json::set('{"example": 12345}', 'example', 56789));
+echo PHP_EOL;
+
+// Example: set array
+$label = 'Set array: ';
+// {"example": 12345} => {"example": 12345, "example2": 56789}
+var_dump($label . Json::setArray('{"example": 12345}', ['example2' => 56789]));
+// {"example": 12345} => {"example": 56789}
+var_dump($label . Json::setArray('{"example": 12345}', ['example' => 56789]));
+echo PHP_EOL;
+
+// Example: get
+$label = 'Get: ';
+// {"example": 12345} => 12345
+var_dump($label . Json::get('{"example": 12345}', 'example'));
+// {"example": 12345} => 56789
+var_dump($label . Json::get('{"example": 12345}', 'example2', 56789));
 echo PHP_EOL;
 
 // Example: error to exception
@@ -28,39 +58,43 @@ $json = [
 ];
 
 // Example: is not error
+$label = 'Is not error: ';
 foreach ($json as $string) {
     Json::decode($string);
-    var_dump(Json::isNoteError());
+    var_dump($label . Json::isNoteError());
 }
 echo PHP_EOL;
 
 // Example: get error msg
+$label = 'Get error msg: ';
 foreach ($json as $string) {
     Json::decode($string);
-    var_dump(Json::getErrorMsg('(json)'));
+    var_dump($label . Json::getErrorMsg('(json)'));
 }
 echo PHP_EOL;
 
-// Example: error to exception
+// Example: error to exception status
+$label = 'Error to exception status: ';
 foreach ($json as $string) {
     try {
         Json::decode($string);
         Json::errorToException();
-        var_dump('success');
+        var_dump($label . 'success');
     } catch (CSEHelpersJsonException $e) {
-        var_dump($e->getMessage());
+        var_dump($label . $e->getMessage());
     }
 }
 echo PHP_EOL;
 
-// Exception set check exception
+// Example: exception set check exception
+$label = 'Exception set check exception: ';
 Json::setCheckException();
 foreach ($json as $string) {
     try {
         Json::decode($string);
-        var_dump('success');
+        var_dump($label . 'success');
     } catch (CSEHelpersJsonException $e) {
-        var_dump($e->getMessage());
+        var_dump($label . $e->getMessage());
     }
 }
 echo PHP_EOL;
